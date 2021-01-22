747533e3bc86059968aee7ac0dca980b8a97504c
ASoTNetworks <dev@aseriesoftubez.com>
2021-01-21 20:16:15 -0500

    remove tar from deploy.sh

--- .distributed-press/deploy.sh
+++ .distributed-press/deploy.sh
@@ -8,8 +8,5 @@ PROJECT_WWW=$3
 # Configure the site
 curl -v https://api.distributed.press/v0/publication/configure -H "Content-Type: multipart/form-data" -H "Accept: application/json" -H "Authorization: Bearer ${PROJECT_API_KEY}" -F "file=@${PROJECT_CONFIG}"
 
-# Tar the website
-tar -czvf www.tar.gz -C _site .
-
 # Publish the website
 curl -v https://api.distributed.press/v0/publication/publish -H "Content-Type: multipart/form-data" -H "Accept: application/json" -H "Authorization: Bearer ${PROJECT_API_KEY}" -F "file=@${PROJECT_WWW}"
