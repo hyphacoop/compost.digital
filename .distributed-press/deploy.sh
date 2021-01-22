af1aed58d766802a99d5a60b238fa9ff13c7c111
ASoTNetworks <dev@aseriesoftubez.com>
2021-01-21 20:10:47 -0500

    Update deploy.sh to support different config and www archive

--- .distributed-press/deploy.sh
+++ .distributed-press/deploy.sh
@@ -2,12 +2,14 @@
 
 # Get values from arguments
 PROJECT_API_KEY=$1
+PROJECT_CONFIG=$2
+PROJECT_WWW=$3
 
 # Configure the site
-curl -v https://api.distributed.press/v0/publication/configure -H "Content-Type: multipart/form-data" -H "Accept: application/json" -H "Authorization: Bearer ${PROJECT_API_KEY}" -F "file=@./.distributed-press/config.json"
+curl -v https://api.distributed.press/v0/publication/configure -H "Content-Type: multipart/form-data" -H "Accept: application/json" -H "Authorization: Bearer ${PROJECT_API_KEY}" -F "file=@${PROJECT_CONFIG}"
 
 # Tar the website
 tar -czvf www.tar.gz -C _site .
 
 # Publish the website
-curl -v https://api.distributed.press/v0/publication/publish -H "Content-Type: multipart/form-data" -H "Accept: application/json" -H "Authorization: Bearer ${PROJECT_API_KEY}" -F "file=@www.tar.gz"
+curl -v https://api.distributed.press/v0/publication/publish -H "Content-Type: multipart/form-data" -H "Accept: application/json" -H "Authorization: Bearer ${PROJECT_API_KEY}" -F "file=@${PROJECT_WWW}"
